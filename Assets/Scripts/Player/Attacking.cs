9e4fd8b2bdcba2ca4d48773ef50d4caa290226fd
Gabriel Cochard <gcochard@artfx.fr>
2023-03-17 18:57:08 +0100

    Struggling
    Struggling with animation...
    Trying to explore particles system. Maybe.

--- Assets/Scripts/Player/Attacking.cs
+++ Assets/Scripts/Player/Attacking.cs
@@ -6,13 +6,59 @@ public class Attacking : MonoBehaviour
 {
     public GameObject Sword;
 
-    void Update()
+    private bool Abilityy = false;
+
+    private Animator animator;
+
+    private void Start()
+    {
+       Animator anim = GetComponent<Animator>();
+    }
+
+    /*void Update()
     {
         if (Input.GetMouseButton(0))
         {
             Debug.Log("Pressed left click.");
-            
-            Sword.GetComponent<Animator>().Play("AbilityT");
+
+            animator.SetBool("Abilityy", true);
+
+
+            //Sword.GetComponent<Animator>().Play("AbilityT");
+
+            //itAnim();
+        }
+
+        else
+        {
+            animator.SetBool("Abilityy", false);
+        }
+    }*/
+    /*private void HitAnim()
+    {
+        if (Att)
+        {
+            animator.SetBool("Att", true);
+        }
+
+        else
+        {
+            animator.SetBool("Att", false);
+        }*/
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Eurêka");
+            //fire = true;
+            animator.SetBool("Abilityy", true);
+        }
+
+        else
+        {
+            animator.SetBool("Abilityy", false);
         }
     }
 }
