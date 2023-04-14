70d72a1c7b190209a7560e076de275e229da79b4
Gabriel Cochard <gcochard@artfx.fr>
2023-04-14 19:40:46 +0200

    New scene test
    -Testing camera animation.
    -Sword animation speed changed a lil bit.

---
+++ Assets/Scripts/Player/CamAnimTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamAnimTest : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click");
+        }
+    }
+}
