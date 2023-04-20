5bfb52565108f522d88ba4645fbff2e5a63155e5
Gabriel Cochard <gcochard@artfx.fr>
2023-04-20 15:20:31 +0200

    [CODE] - New detection for falling
    -Script to detect if the tree if moving is now functional.
    -Still needs some polish
    /!\ Lots of errors when the gameobject is destroyed.

--- Assets/Scripts/Tree/TreeHit.cs
+++ Assets/Scripts/Tree/TreeHit.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class TreeHit : MonoBehaviour
 {
-
+    public Rigidbody rb;
     public GameObject Sound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.x > 0 || rb.velocity.y > 0)
+        {
+            Debug.Log("It's moving");
+            Sound.SetActive(true);
+            Destroy(Sound, 5);
+        }
     }
 
-    void OnTriggerEnter(Collider collision)
+    /*void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Projectile"))
         {
@@ -34,5 +41,5 @@ public class TreeHit : MonoBehaviour
             Sound.SetActive(true);
             Destroy(Sound, 5);
         }
-    }
+    }*/
 }
