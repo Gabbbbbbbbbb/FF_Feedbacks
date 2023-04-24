cf149c0dac7113d7fd105df68245d9158c39a6b0
Gabbbbbbbbbb <gcochard@artfx.fr>
2023-04-24 20:34:31 +0200

    [CODE] & [DEV]
    -Text mesh pro.
    -Trying to code for the score (it's difficult okay).

--- Assets/Scripts/Tree/ScoreTree.cs
+++ Assets/Scripts/Tree/ScoreTree.cs
@@ -1,26 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Text ValueText;
+    int ScoreValue = 1;
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Terrain"))
         {
-            Debug.Log("Into da ground");
+            ValueText.text = ("ScoreValue");
         }
     }
 }
