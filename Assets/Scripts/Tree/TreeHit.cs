281af37028d9ea65c66e0f30a3c6b4b216bcc3c7
Gabbbbbbbbbb <gcochard@artfx.fr>
2023-04-25 23:23:00 +0200

    [CODE] & [UI] - Displaying score and updating it
    -Score is now only focused on the "TreeHit" script.
    -Need to revamp the tree prefab (horrible as it is) / find a way to change prefabs settings quickly.
    -Deleted the "ScoreTree" script.

--- Assets/Scripts/Tree/TreeHit.cs
+++ Assets/Scripts/Tree/TreeHit.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TreeHit : MonoBehaviour
 {
     public Rigidbody rb;
     public GameObject Sound;
+
+    //For the score.
+    public TextMeshProUGUI ScoreText;
+    int Value = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         //rb.velocity = Vector3.zero;
     }
 
@@ -23,6 +29,18 @@ public class TreeHit : MonoBehaviour
             Debug.Log("It's moving");
             Sound.SetActive(true);
             Destroy(Sound, 5);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Terrain"))
+        {
+            //Debug.Log("The ground");
+            Value = Value + 1;
+            ScoreText.text = Value.ToString();
+            
+
         }
     }
 
