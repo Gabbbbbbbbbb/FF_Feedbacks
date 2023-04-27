f53d39c56ae25157197cb1b417068aaf7fddc764
Gabriel Cochard <gcochard@artfx.fr>
2023-04-27 21:13:51 +0200

    [CODE] & [UI] - Slider anim (works)
    -Working on "changing" the sliders once max number is reached.
    -New anime for the slider "change" (in a new folder). Script with trigger works.
    
    Now, I need to focus on the display / reward of the slider + point (cf : google sheet).

--- Assets/Scripts/GameManagerUI.cs
+++ Assets/Scripts/GameManagerUI.cs
@@ -17,12 +17,16 @@ public class GameManagerUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI ScoreText;
     int Value = 0;
 
+    public Animator m_Animator;
+
     //This script gets called by the TreeHit script to update the score and Slider.
     void Start()
     {
         RepSlider.maxValue = MaxSlider;
         RepSlider.minValue = MinSlider;
         CurrentSlider = MinSlider;
+
+        m_Animator.gameObject.GetComponent<Animator>();
     }
 
     //Called by the TreeHit script.
@@ -34,5 +38,11 @@ public class GameManagerUI : MonoBehaviour
 
         Value++;
         ScoreText.text = Value.ToString();
+
+        if(CurrentSlider >= 5)
+        {
+            //Debug.Log("Plus");
+            m_Animator.SetTrigger("Achieved");
+        }
     }
 }
