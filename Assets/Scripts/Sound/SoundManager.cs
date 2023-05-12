4d3f71ac33d516e3805b7e9de1a070e4f85b0d54
Gabriel Cochard <gcochard@artfx.fr>
2023-05-12 19:35:25 +0200

    [UI] & [CODE] - Sound Slider
    -Created a script in a new folder to manage the sound (via AudioListener present on the camera) by using a slider.
    /!\ Need to create a "close button" that will re-lock the mouse and close the menu.

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioListener Listen;
+    [SerializeField] private Slider s_slider;
+    [SerializeField] private float maxSlider = 1.0f;
+    [SerializeField] private float minSlider = 0.0f;
+
+    // No need for custom currentValue since we can say that AudioListener.volume equals to the slider directly. Less code.
+    void Start()
+    {
+        s_slider.maxValue = maxSlider;
+        s_slider.minValue = minSlider;
+        AudioListener.volume = s_slider.value;
+    }
+
+    void Update()
+    {
+        AudioListener.volume = s_slider.value;
+    }
+}
