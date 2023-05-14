f31e0e77975ccbedb81af8c911e9655d43777389
Gabbbbbbbbbb <gcochard@artfx.fr>
2023-05-14 22:56:00 +0200

    [CODE] - Sound spawn fix
    -Fixed the way of activating sound when tree falling. No more thousands of errors now.

--- Assets/Scripts/Tree/TreeHit.cs
+++ Assets/Scripts/Tree/TreeHit.cs
@@ -8,35 +8,36 @@ public class TreeHit : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private GameObject Sound;
+    private bool Once = true;
 
     [Header("GameManager Link")]
     public GameManagerUI GameManagerUI;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //Used to detect if something is moving (using his Rigidbody).
         if (rb.velocity.x > 0 || rb.velocity.y > 0)
         {
-            //Debug.Log("It's moving");
-            Sound.SetActive(true);
-            Destroy(Sound, 5);
+            if (Once == true)
+            {
+                Once = false;
+                Sound.SetActive(true);
+                Destroy(Sound, 5);
+            }
         }
     }
 
+    // Script to earn score points when a collider on the tree hits the ground.
     void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Terrain"))
         {
-            //Debug.Log("The ground");
             GameManagerUI.Gain();
         }
     }
-    //bool pour indiquer quelle slider est actif ou non. À vérif dès qu'il y a collision.
 }
